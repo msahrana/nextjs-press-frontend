50d2f3aa6ebb9e03b79a8d2175b32dc77aad8b0b
Sayed Anower Hossain Rana <msahrana@gmail.com>
2026-07-25 08:40:41 +0600

    post form dialog form updated

--- app/(dashboardGroup)/_components/PostFormDialog.tsx
+++ app/(dashboardGroup)/_components/PostFormDialog.tsx
@@ -18,6 +18,7 @@ import { IPost } from '@/lib/types';
 import { PencilIcon, PlusIcon } from 'lucide-react';
 import { useActionState, useEffect, useState } from 'react';
 import { toast } from 'sonner';
+import { createPost, updatePost } from '../_actions/myPostsActions';
 
 type PostFormDialogProps = {
     mode: 'create' | 'edit';
@@ -27,7 +28,8 @@ type PostFormDialogProps = {
 export function PostFormDialog({ mode, post }: PostFormDialogProps) {
     const [open, setOpen] = useState(false);
 
-    const action = mode === 'edit' && post ? () => {} : () => {};
+    const action =
+        mode === 'edit' && post ? updatePost.bind(null, post.id) : createPost;
 
     const [state, formAction, pending] = useActionState(action, null) as any;
 
